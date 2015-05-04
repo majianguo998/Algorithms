c507822fc76c2c645ab97f7ec4c1737cf64ddae0
majianguo998 <616763780@qq.com>
2015-05-04 21:27:55 +0800

    From Intro of Algo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "QuickSort.h"
 #include "InsertSort.h"
+#include "InsertSort1.h"
 using namespace std;
 
 int main()
 {
     const int N = 6;
-    int a[N] = {8,4,7,0,1,4};
-    InsertSort(a,N);
+    int a[N] = {8,4,7,100,5,4};
+    //InsertSort(a,N);
+    InsertSort1(a,0,N-1);
     for(int i=0;i<N;i++)
         cout << a[i] << " ";
     return 0;
