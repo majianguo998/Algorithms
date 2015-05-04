b7e4d25b896c934838a8f97a7e0e1a3ac35885e8
majianguo998 <616763780@qq.com>
2015-05-04 19:56:03 +0800

    sort

---
+++ HeapSort.h
@@ -0,0 +1,6 @@
+#ifndef HEAPSORT_H_INCLUDED
+#define HEAPSORT_H_INCLUDED
+
+
+
+#endif // HEAPSORT_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "QuickSort.h"
+#include "InsertSort.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    const int N = 6;
+    int a[N] = {8,4,7,0,1,4};
+    InsertSort(a,N);
+    for(int i=0;i<N;i++)
+        cout << a[i] << " ";
     return 0;
 }
