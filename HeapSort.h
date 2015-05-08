f3e6d97029dbe880f94cdd05e0a29f4cb8a85598
majianguo998 <616763780@qq.com>
2015-05-08 21:07:56 +0800

    al

--- HeapSort.h
+++ HeapSort.h
@@ -1,6 +1,58 @@
 #ifndef HEAPSORT_H_INCLUDED
 #define HEAPSORT_H_INCLUDED
 
+/**
+*维护堆的性质
+*数组下标从0开始，父结点为i，左孩子结点为2i+1,右孩子结点为2i+2。
+*child is i,father is (i-1)/2
+*/
+void MaxHeapIFY(int* a,int i,int heap_size)
+{
+    int left = 2*i+1;
+    int right = 2*i+2;
+    int largest;
+    if(left<heap_size && a[left]>a[i])
+        largest = left;
+    else largest = i;
+    if(right<heap_size && a[right]>a[largest])
+        largest = right;
+    if(largest != i)
+    {
+        int temp = a[i];
+        a[i] = a[largest];
+        a[largest] = temp;
 
+        MaxHeapIFY(a,largest,heap_size);
+    }
+
+
+}
+
+void buildMaxHeap(int* a,int N)
+{
+    int size = N;
+    int heap_size = size;
+    for(int i= size/2-1;i>=0;i--)
+    {
+        MaxHeapIFY(a,i,heap_size);
+    }
+}
+
+void HeapSort(int* a,int N)
+{
+    buildMaxHeap(a,N);
+    int size = N;
+    int heap_size = size;
+    for(int i=size-1;i>0;i--)
+    {
+        int temp = a[i];
+        a[i] = a[0];
+        a[0] = temp;
+
+        --heap_size;
+
+        MaxHeapIFY(a,0,heap_size);
+    }
+}
 
 #endif // HEAPSORT_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -3,15 +3,22 @@
 #include "InsertSort.h"
 #include "InsertSort1.h"
 #include "MergeSort.h"
+#include "HeapSort.h"
 using namespace std;
 
 int main()
 {
-    const int N = 8;
-    int a[N] = {8,4,7,100,5,4,2,9};
+    const int N = 10;
+    int a[N] = {8,4,7,100,5,4,2,9,80,4000};
+    int* b = a;
+    int b_size = sizeof(b)/sizeof(b[0]);
+    cout<<"b size = "<<b_size<<endl;
+    int size = sizeof(a)/sizeof(a[0]);
+    cout<<"a size = "<<size<<endl;
     //InsertSort(a,N);
     //InsertSort1(a,0,N-1);
-    mergeSort(a,0,7);
+    //mergeSort(a,0,7);
+    HeapSort(a,N);
     for(int i=0;i<N;i++)
         cout << a[i] << " ";
     return 0;
